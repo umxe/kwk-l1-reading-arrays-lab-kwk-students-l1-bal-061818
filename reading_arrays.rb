4a96da4ff95d33fa1e0d2d253222b4c8eeab43cc
umxe <u_habiba@rocketmail.com>
2018-06-19 18:52:35 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,4 +1,4 @@
-$STUDENT_NAMES = [
+STUDENT_NAMES = [
   "Adele",
   "Beyoncé",
   "Cardi B",
@@ -8,29 +8,29 @@ $STUDENT_NAMES = [
 ]
 
 def first_student_by_index
-  puts $STUDENT_NAMES[0]
+  puts STUDENT_NAMES[0]
 end
 
 def fourth_student_by_index
-  puts $STUDENT_NAMES[3]
+  puts STUDENT_NAMES[3]
 end
 
 def last_student_by_index
-  puts $STUDENT_NAMES[-1]
+  puts STUDENT_NAMES[-1]
 end
 
 def first_student_by_method
-  puts $STUDENT_NAMES.first
+  puts STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  puts $STUDENT_NAMES.last
+  puts STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
-  puts $STUDENT_NAMES[0]
-  puts $STUDENT_NAMES[1]
-  puts $STUDENT_NAMES[2]
+  puts STUDENT_NAMES[0]
+  puts STUDENT_NAMES[1]
+  puts STUDENT_NAMES[2]
 end
 first_second_and_third_students
 first_student_by_method
